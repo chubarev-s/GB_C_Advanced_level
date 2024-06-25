3c9583024f7d827924b4977c93da5ac4e2376f30
chubarev.s <chubarev_sergey@mail.ru>
2024-06-25 21:54:33 +0500

    hw_5

--- Snake/snake.c
+++ Snake/snake.c
@@ -6,25 +6,43 @@
 #include <time.h>
 
 
-#define MAX_X 15
-#define MAX_Y 15
+#define MAX_X 60
+#define MAX_Y 30
+
+/*Глобальная переменная для паузы*/
+int PAUSE = 0;
+
+
 /*Структура описывающая "хвост змейки" */
 typedef struct tail_t{
 	int x;
 	int y;
 } tail_t;
 
+/*Пречисление направлений*/
+typedef enum{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+} Direction;
+
 /*Структура описывающая змейку*/	
 typedef struct snake_t{
 	int x;
 	int y;
 	int score;
-	int eat;
-	int food_x; 
-	int food_y;
 	struct tail_t * tail;
 	size_t tsize;
+	Direction direction;
 }  snake_t;
+ 
+/*Структура для еды*/
+typedef struct food_t{
+    int x;
+    int y;
+    int isEaten;
+} food_t;
 
 /*Ф-ция возвращающая структуру змейки */
 struct snake_t initSnake(int x, int y, size_t tsize){
@@ -32,30 +50,27 @@ struct snake_t initSnake(int x, int y, size_t tsize){
 	snake.x = x;
 	snake.y = y;
 	snake.tsize = tsize;
-	snake.eat = 0;
 	snake.tail = (tail_t *) malloc (sizeof(tail_t) * 100);
 	for (int i =0; i < tsize; ++i){
 		snake.tail[i].x = x + i +1;
 		snake.tail[i].y = y;
-		}
+	}
+	snake.direction = LEFT;  //задали начальное движение
 	return snake;
 }
 
-/*задаём функцию случайной генерации чисел*/
-int getRandomNumber(int min, int max) {
- 	return min + rand() % (max - min +1);
-}
-	
-/*Функция инициализации символа "Q", еды*/
-void initFoodPosition(snake_t *snake) {
-	snake->food_x = getRandomNumber(0, (MAX_X-1));
-	snake->food_y = getRandomNumber(0, (MAX_Y-1));
+/*Ф-ция иниц. еды*/
+food_t initFood(){
+    food_t food;
+    food.x = rand () % MAX_X;
+    food.y = rand () % MAX_Y;
+    food.isEaten = 0;
+    return food;
 }
 
 /* Рисуем змейку вида: @** */
-uint8_t printSnake(struct snake_t *snake){
+void printSnake(struct snake_t snake, struct snake_t snake2, food_t food){
 		char matrix[MAX_X][MAX_Y];
-		uint8_t ex_w = 0, r_x = 0, r_y = 0;
 		for (int i = 0; i < MAX_X; ++i)
 		{
 			for (int j = 0; j < MAX_Y; ++j)
@@ -64,126 +79,117 @@ uint8_t printSnake(struct snake_t *snake){
 			}
 		}
 		
-		for (int i = 0; i < snake->tsize; ++i)
+        matrix[food.x][food.y] = 'Q';
+
+		matrix[snake.x][snake.y] = '@';
+		matrix[snake2.x][snake2.y] = '&';
+        for(int i = 0; i < snake.tsize; ++i){
+            matrix[snake.tail[i].x][snake.tail[i].y] = '*';
+        }
+		for(int i = 0; i < snake2.tsize; ++i){
+            matrix[snake2.tail[i].x][snake2.tail[i].y] = '+';
+        }
+		for (int j = 0; j < MAX_Y; ++j)
 		{
-			matrix[snake->tail[i].x][snake->tail[i].y] = '*';
+			for (int i = 0; i < MAX_X; ++i)
+			{
+				printf("%c", matrix[i][j]);
+			}
+            printf("\n");
 		}
-		
-		matrix[snake->food_x][snake->food_y] = 'Q'; 
+}
+	
+/*Движение змейки*/
+snake_t move(snake_t snake, food_t *food){
+    for(int i = snake.tsize; i > 0; i--){
+        snake.tail[i] = snake.tail[i-1];
+    }
 
-		switch (matrix[snake->x][snake->y])
+    snake.tail[0].x = snake.x;
+    snake.tail[0].y = snake.y;
+
+    switch (snake.direction)
+    {
+    case UP:
+        snake.y = snake.y - 1;
+        if (snake.y < 0){
+            snake.y = MAX_Y - 1;
+        }
+        break;
+    case DOWN:
+        snake.y = snake.y + 1;
+        if (snake.y == MAX_Y){
+            snake.y = 0;
+        }
+        break;
+    case LEFT:
+        snake.x = snake.x - 1;
+        if (snake.x < 0){
+            snake.x = MAX_X - 1;
+        }
+        break;
+    case RIGHT:
+        snake.x = snake.x + 1;
+        if (snake.x == MAX_X){
+            snake.x = 0;
+        }
+        break;
+    }
+    if(snake.x == food -> x && snake.y == food -> y){
+        food -> isEaten = 1;
+        snake.tsize++;
+		snake.score++;
+    }
+    return snake;
+}
+
+//для управления второй змейкой убрать комментарии ниже:
+/*Ф-ция движения*/
+void input(snake_t *snake){//, snake_t *snake2){
+    if(_kbhit()){
+        switch (_getch())
 		{
-			case ' ':
-				matrix[snake->x][snake->y] = '@'; 
-				for (int j = 0; j < MAX_Y; ++j)
-				{
-					for (int i = 0; i < MAX_X; ++i)
-					{
-						printf("%c", matrix[i][j]);
-					}
-					printf("\n");
-				}
+			case 'a':
+			case 'A':
+                snake->direction = LEFT;
 				break;
-			case '*':
-				printExit(snake);
-				return 1;
+			case 'w':
+			case 'W':
+                snake->direction = UP;
 				break;
-			case 'Q':
-				snake->tsize = snake->tsize + 1;
-				snake->score = snake->score + 1;
-				matrix[snake->food_x][snake->food_y] = ' ';
-				matrix[snake->x][snake->y] = '@';
-				for (int j = 0; j < MAX_Y; ++j)
-				{
-					for (int i = 0; i < MAX_X; ++i)
-					{
-						printf("%c", matrix[i][j]);
-					}
-					printf("\n");
-				}
-				snake->eat = 0;
-				initFoodPosition(snake); // Генерируем новое положение символа "Q"
+    		case 'd':
+			case 'D':
+			    snake->direction = RIGHT;	
 				break;
-			default:
-				printf("error, %c", matrix[snake->x][snake->y]);
+			case 's':
+			case 'S':
+				snake->direction = DOWN;
 				break;
-		}
-		return 0;
-	}
-	
-/*Движение змейки влево*/
-snake_t moveLeft(snake_t snake){
-	for (int i = snake.tsize - 1; i > 0; i--)
-	{
-		snake.tail[i] = snake.tail[i-1];          //теряем последнюю "часть" хвоста, сдвигая всё влево
-	}
-	snake.tail[0].x = snake.x;
-	snake.tail[0].y = snake.y;
-	
-	snake.x = snake.x - 1;	
-	/*чтобы змейка не исчезала (и не словили ошибку фрагминтации, т.к. это уход за выделенную память), а выходила с другого края:*/
-	if (snake.x < 0)
-	{
-		snake.x = MAX_X - 1;
-	}
-	 return snake;
-}	
+			case 'p':
+			case 'P':
+				PAUSE = 1;
+				break;
+			// case 'j':
+			// case 'J':
+            //     snake2->direction = LEFT;
+			// 	break;
+			// case 'i':
+			// case 'I':
+            //     snake2->direction = UP;
+			// 	break;
+    		// case 'l':
+			// case 'L':
+			//     snake2->direction = RIGHT;	
+			// 	break;
+			// case 'k':
+			// case 'K':
+			// 	snake2->direction = DOWN;
+			// 	break;
+        }
+    }
+}
 
-/*Движение змейки вправо*/
-snake_t moveRight(snake_t snake){
-	for (int i = snake.tsize - 1; i > 0; i--)
-	{
-		snake.tail[i] = snake.tail[i-1];          
-	}
-	snake.tail[0].x = snake.x;
-	snake.tail[0].y = snake.y;
-	
-	snake.x = snake.x + 1;	
-	/*чтобы змейка не исчезала (и не словили ошибку фрагминтации, т.к. это уход за выделенную память), а выходила с другого края:*/
-	if (snake.x > MAX_X - 1)
-	{
-		snake.x = 0;
-	}
-	 return snake;
-}	
-
-/*Движение змейки вниз*/
-snake_t moveDown(snake_t snake){
-	for (int i = snake.tsize - 1; i > 0; i--)
-	{
-		snake.tail[i] = snake.tail[i-1];          
-	}
-	snake.tail[0].x = snake.x;
-	snake.tail[0].y = snake.y;
-	
-	snake.y = snake.y + 1;	
-	/*чтобы змейка не исчезала (и не словили ошибку фрагминтации, т.к. это уход за выделенную память), а выходила с другого края:*/
-	if (snake.y > MAX_Y - 1)
-	{
-		snake.y = 0;
-	}
-	 return snake;
-}	
-
-/*Движение змейки вверх*/
-snake_t moveUp(snake_t snake){
-	for (int i = snake.tsize - 1; i > 0; i--)
-	{
-		snake.tail[i] = snake.tail[i-1];          
-	}
-	snake.tail[0].x = snake.x;
-	snake.tail[0].y = snake.y;
-	
-	snake.y = snake.y - 1;	
-	/*чтобы змейка не исчезала (и не словили ошибку фрагминтации, т.к. это уход за выделенную память), а выходила с другого края:*/
-	if (snake.y < 0)
-	{
-		snake.y = MAX_Y - 1;
-	}
-	 return snake;
-}	
-
-
+ 
 /*Функция выхода*/
 void printExit(snake_t *head){
 	char matrix[MAX_X][MAX_Y];
@@ -196,17 +202,51 @@ void printExit(snake_t *head){
 		}
 	
 	printf("GAME OWER! Your score %d\n", head->score);
-	*head = initSnake( 10, 5, 5);
-	printSnake(head);
 }
 
+/*Бот для второй змейки*/
+void generateSnakeDirection(snake_t *snake, food_t food){
+	if ((snake->direction == LEFT || snake->direction == RIGHT) && snake->y == food.y){
+		return;
+	}
+	if ((snake->direction == LEFT || snake->direction == RIGHT) && snake->y != food.y){
+		if (food.x == snake->x){
+			if(abs(food.y - snake->y) <= MAX_Y/2)
+			{
+				snake->direction = food.y - snake->y > 0? DOWN:UP;
+			} else {
+				snake->direction = food.y - snake->y > 0? UP:DOWN;
+			}
+		}
+		return;
+	}
+	if ((snake->direction == UP || snake->direction == DOWN) && snake->x == food.x){
+		return;
+	}
+	if ((snake->direction == UP || snake->direction == DOWN) && snake->x != food.x){
+		if (food.y == snake->y){
+			if(abs(food.x - snake->x) <= MAX_X/2)
+			{
+				snake->direction = food.x - snake->x > 0? RIGHT:LEFT;
+			} else {
+				snake->direction = food.x - snake->x > 0? LEFT:RIGHT;
+			}
+		}
+		return;
+	}
+}
+
+
 int main(){
-	int a = 0, exit_while = 0, close = 0;
-	char key, last_key = 'a', key_menu;
-	struct snake_t snake = initSnake( 10, 5, 1);
+	int exit_while = 0, close = 0;
+	char key_menu;
+	
+	struct snake_t snake = initSnake( 10, 5, 2);
+	struct snake_t snake2 = initSnake( 15, 17, 2);
 	snake.score = 0;
-	initFoodPosition(&snake);
-	printSnake(&snake);
+	snake2.score = 0;
+	food_t food = initFood();
+	printSnake(snake, snake2, food);
 	while (close != 1)
 	{
 		printf("1. Start game\n");
@@ -218,72 +258,35 @@ int main(){
 			exit_while = 0;
 			while(exit_while != 1)
 			{
-				if(kbhit())
-				{
-					key = getch(); 
-					if ((key == 'w')||(key == 'W')||(key == 'a')||(key == 'A')||(key == 's')||(key == 'S')||(key == 'd')||(key == 'D'))
+				input(&snake);//, &snake2);
+                snake = move(snake, &food);
+				snake2 = move(snake2, &food);
+				generateSnakeDirection(&snake2, food);
+                if(food.isEaten){
+                    food = initFood();
+                }
+				if(PAUSE == 1){
+					printf("Pause!\n Your score: %d\n Bot score: %d\n", snake.score, snake2.score);
+					printf("Press P to continue or E to exit\n");
+					while (PAUSE != 0)
 					{
-						switch (key)
-						{
-						case 'a':
-						case 'A':
-							snake = moveLeft(snake);
-							last_key = 'a';
-							break;
-						case 'w':
-						case 'W':
-							snake = moveUp(snake);
-							last_key = 'w';
-							break;
-						case 'd':
-						case 'D':
-							snake = moveRight(snake);
-							last_key = 'd';
-							break;
-						case 's':
-						case 'S':
-							snake = moveDown(snake);
-							last_key = 's';
-							break;
-						default:
-							break;
+						if(_kbhit()){
+        					switch (_getch()){
+								case 'P':
+								case 'p':
+									PAUSE = 0;
+									break;
+								case 'e':
+								case 'E':
+									exit_while = 1;
+									PAUSE = 0;
+									break;
+							}
 						}
 					}
+					
 				}
-				else if(key == 'p'){
-					printf("Pause\nYour score: %d\nClick any key for contine or \"e\" for exit\n", snake.score);
-					key = getch();
-					if(key == 'e')
-						exit_while = 1;
-				}else 
-					{  
-						switch (last_key)
-						{
-						case 'a':
-						case 'A':
-							snake = moveLeft(snake);
-							key = 'a';
-							break;
-						case 'w':
-						case 'W':
-							snake = moveUp(snake);
-							key = 'w';
-							break;
-						case 'd':
-						case 'D':
-							snake = moveRight(snake);
-							key = 'd';
-							break;
-						case 's':
-						case 'S':
-							snake = moveDown(snake);
-							key = 's';
-							break;
-						default:
-							break;
-						}
-				}
-				if(snake.score < 4){
+				 if(snake.score < 4){
 					usleep(400000);
 				}
 				if((snake.score >= 4)&&(snake.score < 7)){
@@ -296,10 +299,7 @@ int main(){
 					usleep(50000);
 				}
 				system("cls");
-				if(exit_while !=1 )
-				{
-					exit_while = printSnake(&snake);
-				}
+                printSnake(snake, snake2, food);
 			}
 			break;
 		case '2':
